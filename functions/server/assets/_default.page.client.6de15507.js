2641d2b05bdd9533ad30ad56de9053289124252a
prfctbeing <prfctbeing@gmail.com>
2023-04-25 16:09:54 -0400

    adding functions

---
+++ functions/server/assets/_default.page.client.6de15507.js
@@ -0,0 +1,2 @@
+"use strict";
+throw new Error("[vite-plugin-ssr][Wrong Usage] File C:/Users/ellio/Desktop/vite-plugin-ssr-cloudflare-pages-demo/renderer/_default.page.client.ts should not be loaded in Node.js");

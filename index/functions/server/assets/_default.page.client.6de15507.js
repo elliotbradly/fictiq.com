02f227ed16421eae22f61d5eb6d0289bf103311f
prfctbeing <prfctbeing@gmail.com>
2023-04-25 16:12:24 -0400

    try that

--- index/functions/server/assets/_default.page.client.6de15507.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-throw new Error("[vite-plugin-ssr][Wrong Usage] File C:/Users/ellio/Desktop/vite-plugin-ssr-cloudflare-pages-demo/renderer/_default.page.client.ts should not be loaded in Node.js");

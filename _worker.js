dd428f05fce1c2e09416d178607c7f0fdcebb3fc
prfctbeing <prfctbeing@gmail.com>
2023-05-25 19:36:52 -0400

    we can share information

--- _worker.js
+++ _worker.js
@@ -3,6 +3,29 @@ export default {
         const url = new URL(request.url);
         if (url.pathname.startsWith('/api/')) {
             // TODO: Add your custom /api/* logic here.
+
+            switch (url.pathname) {
+                case '/openService':
+                    bit = await globalThis.TASUS.hunt(globalThis.TASUS.ActSrv.OPEN_SERVICE, { idx });
+                    value = bit;
+                    break
+                case '/update':
+                    value = Number(value) + 1
+                    break
+                case '/reset':
+                    value = 555
+                    break
+                case '/':
+                    value = 'change';
+                    break
+                default:
+                    return new Response('Not found', { status: 404 })
+            }
+
+
+
+
+
             return new Response('Ok');
         }
         // Otherwise, serve the static assets.

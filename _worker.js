429f843ac80d3b93f83e35491568f5aa35b75fad
prfctbeing <prfctbeing@gmail.com>
2023-05-26 16:59:53 -0400

    update

--- _worker.js
+++ _worker.js
@@ -3,11 +3,29 @@ export default {
 
     async fetch(request, env) {
         const url = new URL(request.url);
+
+        var idx = url.searchParams.get('idx')
+        var src = url.searchParams.get('src')
+        var val = url.searchParams.get('val')
+
+        if (val == null) val = 0;
+
         if (url.pathname.startsWith('/auth/')) {
 
-            var jwt = require('@tsndr/cloudflare-worker-jwt')
-            const token = await jwt.sign({ name: 'John Doe', email: 'john.doe@gmail.com' }, 'secret')
-            return new Response(JSON.stringify(token));
+            var neo = 'https://003-light.beeing.workers.dev/fetchColor?val=' + val;
+            neo = encodeURI(neo);
+            //url = url.substring(0, url.length - 1);
+            console.log(neo)
+            bit = await fetch(url, { mode: "no-cors", method: 'GET' })
+            var dat = await bit.json()
+
+            console.log('sjow ' + dat)
+
+            //we get a color
+
+            //var jwt = require('@tsndr/cloudflare-worker-jwt')
+            //const token = await jwt.sign({ name: 'John Doe', email: 'john.doe@gmail.com' }, 'secret')
+            return new Response(JSON.stringify(dat));
 
         } else if (url.pathname.startsWith('/api/')) {
             // TODO: Add your custom /api/* logic here.

112ff41e7b51cff06671890dfbcb55a2b4065759
Brad Henderson <elliotbradly@gmail.com>
2023-06-07 17:04:54 -0400

    new stuff

--- _worker.js
+++ _worker.js
@@ -1,10 +1,8 @@
 export default {
-
-
     async fetch(request, env) {
-
         var light = require('./003.light.js')
         var tasus = require('./015.tasus.js')
+        var machine = require('./555.machine.js')
 
         const url = new URL(request.url)
 
@@ -16,17 +14,21 @@ export default {
         if (val == null) val = 0
 
         if (url.pathname.startsWith('/auth/')) {
-
-            bit = await globalThis.LIGHT.hunt(globalThis.LIGHT.ActClr.FETCH_COLOR, { val })
+            bit = await globalThis.LIGHT.hunt(
+                globalThis.LIGHT.ActClr.FETCH_COLOR,
+                { val }
+            )
 
             return new Response(JSON.stringify({ idx: 'auth', dat: bit }))
-        }
-        else if (url.pathname.startsWith('/open/')) {
-
-            bit = await globalThis.TASUS.hunt(globalThis.TASUS.ActSrv.OPEN_SERVICE, { idx });
+        } else if (url.pathname.startsWith('/open/')) {
+            bit = await globalThis.TASUS.hunt(
+                globalThis.TASUS.ActSrv.OPEN_SERVICE,
+                { idx }
+            )
 
             return new Response(JSON.stringify({ idx: 'open', dat: bit }))
-
+        } else if (url.pathname.startsWith('/black-meat-machine/')) {
+            return new Response(JSON.stringify({ idx: 'black-meat-machine' }))
         }
         // Otherwise, serve the static assets.
         // Without this, the Worker will error and no assets will be served.

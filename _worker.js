88f0b2051d20b0f81f873a303814a97aa143ca7c
Brad Henderson <elliotbradly@gmail.com>
2023-06-09 14:55:12 -0400

    methods

--- _worker.js
+++ _worker.js
@@ -28,7 +28,17 @@ export default {
 
             return new Response(JSON.stringify({ idx: 'open', dat: bit }))
         } else if (url.pathname.startsWith('/black-meat-machine/')) {
-            return new Response(JSON.stringify({ idx: 'black-meat-machine' }))
+            switch (idx) {
+                case 'readTime':
+                    bit = { idx: 'read-time' }
+                    break
+
+                default:
+                    bit = { idx: 'black-meat-machine' }
+                    break
+            }
+
+            return new Response(JSON.stringify(bit))
         }
         // Otherwise, serve the static assets.
         // Without this, the Worker will error and no assets will be served.

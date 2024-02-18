12b5b74d3ae3a0b5b73604eedd3b7c3882e02f8e
Brad Henderson <elliotbradly@gmail.com>
2024-02-17 22:47:29 -0500

    update

---
+++ functions/api/think.js
@@ -0,0 +1,14 @@
+var pivot = require("./888.market");
+
+export const onRequestGet = async () => {
+  var bit = await MARKET.hunt(MARKET.ActOai.UPDATE_OPENAI, {});
+
+  var response = "yo yo yo";
+  return new Response(response);
+};
+
+// POST requests to /filename with a JSON-encoded body would return "Hello, <name>!"
+export const onRequestPost = async ({ request }) => {
+  const { name } = await request.json();
+  return new Response(`Hello, ${name}!`);
+};

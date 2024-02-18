00cc5f87999d43c4001ec0f82a675fa6ad1eabe7
Brad Henderson <elliotbradly@gmail.com>
2024-02-17 22:56:04 -0500

    update

--- functions/api/think.js
+++ functions/api/think.js
@@ -1,7 +1,7 @@
 var pivot = require("./888.market");
 
 export const onRequestGet = async () => {
-  var bit = await globalThis.MARKET.hunt(MARKET.ActOai.UPDATE_OPENAI, {});
+  //var bit = await globalThis.MARKET.hunt(MARKET.ActOai.UPDATE_OPENAI, {});
 
   var response = "yo yo yo";
   return new Response(response);

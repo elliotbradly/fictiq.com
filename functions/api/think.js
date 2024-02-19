da710fad6391ea5568ebd295976e1c127b7f48a3
Brad Henderson <elliotbradly@gmail.com>
2024-02-19 17:39:37 -0500

    gather response

--- functions/api/think.js
+++ functions/api/think.js
@@ -5,6 +5,25 @@ export const onRequestGet = async (context) => {
 
   var body = { idx: context.env.OPENAI_API_KEY };
 
+  /**
+   * gatherResponse awaits and returns a response body as a string.
+   * Use await gatherResponse(..) in an async function to get the response body
+   * @param {Response} response
+   */
+  async function gatherResponse(response) {
+    const { headers } = response;
+    const contentType = headers.get("content-type") || "";
+    if (contentType.includes("application/json")) {
+      return JSON.stringify(await response.json());
+    } else if (contentType.includes("application/text")) {
+      return response.text();
+    } else if (contentType.includes("text/html")) {
+      return response.text();
+    } else {
+      return response.text();
+    }
+  }
+
   const init = {
     body: JSON.stringify(body),
     method: "POST",

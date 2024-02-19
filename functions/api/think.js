55646ce31b14989b9bced84fffdd8768a807922f
Brad Henderson <elliotbradly@gmail.com>
2024-02-19 17:34:47 -0500

    update

--- functions/api/think.js
+++ functions/api/think.js
@@ -1,10 +1,24 @@
 var pivot = require("./888.market");
 
 export const onRequestGet = async (context) => {
-  var bit = await globalThis.MARKET.hunt(MARKET.ActOai.UPDATE_OPENAI, { idx: context.env.OPENAI_API_KEY });
+  const url = "https://render-com-hjy7.onrender.com/mike";
 
-  var response = "yo yo yo bit" + JSON.stringify(bit);
-  return new Response(response);
+  var body = { idx: context.env.OPENAI_API_KEY };
+
+  const init = {
+    body: JSON.stringify(body),
+    method: "POST",
+    headers: {
+      "content-type": "application/json;charset=UTF-8",
+    },
+  };
+
+  const bit = await fetch(url, init);
+  const results = await gatherResponse(response);
+  return new Response(results, init);
+
+  //var response = "yo yo yo bit" + JSON.stringify(bit);
+  //return new Response(response);
 };
 
 // POST requests to /filename with a JSON-encoded body would return "Hello, <name>!"

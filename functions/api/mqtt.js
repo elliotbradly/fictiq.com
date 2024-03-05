aa856922ad0dc3bdba5c35d2319b2980ababff25
Brad Henderson <elliotbradly@gmail.com>
2024-03-05 13:15:57 -0500

    update

---
+++ functions/api/mqtt.js
@@ -0,0 +1,38 @@
+var pivot = require("./888.market");
+
+export const onRequestGet = async (context) => {
+  var body = { idx: "action-depth" };
+
+  var MQTT = globalThis.MARKET.MQTT;
+  //console.log("mqtt" + MQTT);
+
+  const local = "ws://swamp-fly-448d63614f75.herokuapp.com/";
+  //const localBit = { idx: "local", src: "ws://swamp-fly-448d63614f75.herokuapp.com/" };
+
+  var initMarket = globalThis.MARKET.ActMrk.INIT_MARKET;
+  var openMarket = globalThis.MARKET.ActMrk.OPEN_MARKET;
+  //console.log("act " + initMarket);
+
+  var bit = await globalThis.MARKET.hunt(initMarket, { val: 0, dat: MQTT, src: local });
+  //bit = await globalThis.MARKET.hunt( openMarket, {});
+
+  const init = {
+    body: JSON.stringify(body),
+    method: "GET",
+    headers: {
+      "content-type": "application/json;charset=UTF-8",
+    },
+  };
+
+  //const bit = await fetch(url, init);
+  const results = JSON.stringify(bit);
+  return new Response("results: " + results, init);
+  //var response = "yo yo yo bit" + JSON.stringify(bit);
+  //return new Response(response);
+};
+
+// POST requests to /filename with a JSON-encoded body would return "Hello, <name>!"
+export const onRequestPost = async ({ request }) => {
+  const { name } = await request.json();
+  return new Response(`Hello, ${name}!`);
+};

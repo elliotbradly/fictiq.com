3a8309366083eefe6dd28e5771f4629fd8d059bf
Brad Henderson <elliotbradly@gmail.com>
2024-03-22 13:45:52 -0400

    update

--- app/js/plugins/122.blender.js
+++ app/js/plugins/122.blender.js
@@ -589,7 +589,7 @@ const initActivity = (cpy, bal, ste) => {
             ],
         });
         // Retrieve an access_token from your activity's server
-        const response = await fetch("https://fictiq-com.pages.dev/api/token", {
+        const response = await fetch("./api/token", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
